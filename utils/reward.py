91af29cadf113492f5134c846f3d4f33ea031da0
spark631 <donnynguyen631@gmail.com>
2025-12-08 23:56:43 -0500

    reward

--- utils/reward.py
+++ utils/reward.py
@@ -1,4 +1,18 @@
-from ppo_config.py
-
-def compute_reward(prev_state, state, config):
-    pass
+def compute_reward(events, reward_config):
+    """
+    Compute the total reward based on a list of events and a reward configuration.
+    
+    Args:
+        events (list): List of event strings (e.g., ['chop', 'pickup'])
+        reward_config (dict): Dictionary mapping event names to reward values
+        
+    Returns:
+        float: Total reward
+    """
+    reward = reward_config.get('step_penalty', -0.01)
+    
+    for event in events:
+        if event in reward_config:
+            reward += reward_config[event]
+            
+    return reward
